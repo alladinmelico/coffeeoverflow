42d562d906197701b4cb1d45103216cac210d549
Alladin Melico <melico.alladin@gmail.com>
2020-11-27 23:21:54 +0800

    initialize views

---
+++ resources/views/course/create.blade.php
@@ -0,0 +1,14 @@
+<x-jet-diaglog-modal >
+    <x-slot name="title">
+
+    </x-slot>
+    <x-slot name="content">
+        <form action="" method="post">
+            @csrf
+
+        </form>
+    </x-slot>
+    <x-slot name="footer">
+
+    </x-slot>
+</x-jet-diaglog-modal>
