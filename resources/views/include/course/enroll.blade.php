b133fcb270445044368a0d84e14275fb7c57d1eb
Alladin Melico <melico.alladin@gmail.com>
2020-11-29 11:27:38 +0800

    added UI

--- resources/views/include/course/enroll.blade.php
+++ resources/views/include/course/enroll.blade.php
@@ -1,17 +1,20 @@
-<div>
-    {!! Form::open(['route' => ['enroll'], 'method'=>'POST']) !!}
-        @csrf
-        <div class="flex flex-col">
-            <label for="code" class="mt-4 mb-1 uppercase text-grey-darker text-xs font-bold">Code</label>
+<div class="w-64 flex shadow-lg align-items-end flex-col bg-cover bg-center justify-content bg-white p-6 rounded pt-8 pb-8">
+    <div class='bg-green-200'>
+        {!! Form::open(['route' => ['enroll'], 'method'=>'POST']) !!}
+            @csrf
+            <label for="code" class="uppercase text-grey-darker text-xs font-bold">Code</label>
             <div class="flex flex-row">
-                <span class="flex items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold text-grey-darker"><i class="fa fa-lock mr-2" aria-hidden="true"></i></span>
-                {!! Form::text('code', old('code'), ['class'=> 'bg-grey-lighter text-grey-darker py-2 font-normal rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold']) !!}
+                <div class="flex flex-row">
+                    <span class="flex items-center bg-grey-lighter rounded rounded-r-none text-grey-darker"><i class="fa fa-lock mr-2" aria-hidden="true"></i></span>
+                    {!! Form::text('code', old('code'), ['class'=> 'bg-grey-lighter text-grey-darker py-2 font-normal rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold']) !!}
+                </div>
+                @if ($errors->has('code'))
+                    <small class="text-red italic">{{$errors->first('code')}}</small>
+                @endif
             </div>
-            @if ($errors->has('code'))
-                <small class="text-red italic">{{$errors->first('code')}}</small>
-            @endif
-        </div>
+            <button class="shadow-lg w-full text-white bg-teal-500 hover:bg-teal-400 rounded-full p-3" type="submit">Mag-enroll</button>
 
-        <button class="text-green-400" type="submit">Mag-enroll</button>
-    {!! Form::close() !!}
+        {!! Form::close() !!}
+
+        </div>
 </div>
