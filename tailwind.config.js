2a1785cea4c02542e1724458b0418d64ab470379
Alladin Melico <melico.alladin@gmail.com>
2020-11-29 10:32:24 +0800

    added logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,39 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+	purge: [
+		'./vendor/laravel/jetstream/**/*.blade.php',
+		'./storage/framework/views/*.php',
+		'./resources/views/**/*.blade.php'
+	],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Nunito', ...defaultTheme.fontFamily.sans]
+			}
+		},
+		colors: {
+			red: {
+				light: '#C53030',
+				dark: '#952424'
+			},
 
-    variants: {
-        opacity: ['responsive', 'hover', 'focus', 'disabled'],
-    },
+			blue: {
+				light: '#2B6CB0',
+				dark: '#235890'
+			},
 
-    plugins: [require('@tailwindcss/ui')],
-};
+			yellow: {
+				light: '#F6E05E',
+				dark: '#968839'
+			}
+		}
+	},
+
+	variants: {
+		opacity: ['responsive', 'hover', 'focus', 'disabled']
+	},
+
+	plugins: [require('@tailwindcss/ui')]
+}
